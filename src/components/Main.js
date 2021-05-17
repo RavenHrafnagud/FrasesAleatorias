883ba6752fce1a7e15f56e6794220a05e0fec7f1
Raven Hrafnagud <Hrafnfreistudrr@gmail.com>
2021-05-17 03:55:27 -0500

    codigo limpio y terminado prosigo a la realizacion del entorno grafico

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,58 +3,8 @@ import {useAleatorio} from '../hooks/useAleatorio';
 import {useEscribir} from '../hooks/useEscribir';
 import {useIntervalo} from '../hooks/useIntervalo';
 
-// import Frases from '../BsDatos/Bd.json';
 
 function Main() {
-
-    //funcional------------
-    // function aleatorio(){
-    //     var frase=[];
-    //     frase[0]=(Frases.quotes[Math.floor(Math.random()*Frases.quotes.length)]);
-    //     var cita=frase[0].quote;
-    //     var autor=frase[0].author;
-    //     var link=`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${autor}`;
-    //     return{cita,autor,link};
-    // };
-
-
-
-    // var frase=[];
-    // frase[0]=(Frases.quotes[Math.floor(Math.random()*Frases.quotes.length)])
-    // var cita=frase[0].quote
-    // var autor=frase[0].author
-    // var link=`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${autor}`
-
-
-
-  //  function escribir (){
-        // frase[0]=(Frases.quotes[Math.floor(Math.random()*Frases.quotes.length)])
-        // cita=frase[0].quote
-        // autor=frase[0].author
-        // link="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="+autor
-
-        // document.getElementById("text").innerText=cita
-        // document.getElementById("author").innerText=autor
-        // document.getElementById("tweet-quote").setAttribute("href",link)
-        // console.log(cita,autor,link); 
-
-
-
-        //funcional-----------
-        // const {cita,autor,link}=useAleatorio();
-
-        // document.getElementById("text").innerText=cita;
-        // document.getElementById("author").innerText=autor;
-        // document.getElementById("tweet-quote").setAttribute("href",link);
-  //  };
-
-    //funcional---------
-    // setInterval(function(){
-    //     const {cita,autor,link}=useAleatorio();
-    //     document.getElementById("text").innerText=cita;
-    //     document.getElementById("author").innerText=autor;
-    //     document.getElementById("tweet-quote").setAttribute("href",link);
-    // },10000,"JavaScript");
 
 
     useIntervalo();
